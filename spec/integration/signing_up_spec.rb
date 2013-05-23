88fdeb003ce113dc9f1a44fb4e64d7a2c89cf464
Helen Canzler <helen.t.canzler@gmail.com>
2013-05-22 18:38:35 -0700

    Fix signing up feature, take user to alternative confirmation page

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
   feature 'Signing up' do
     scenario 'Successful sign up' do
+
     visit '/'
     click_link 'Sign up'
     fill_in "Email", :with => "user@ticketee.com"
     fill_in "Password", :with => "password"
     fill_in "Password confirmation", :with => "password"
     click_button "Sign up"
-    message = "Please open the link to activate your account."
+    #message = "Please open the link to activate your account."
+    message = "Please confirm your account before signing in."
     page.should have_content(message)
   end
 end
